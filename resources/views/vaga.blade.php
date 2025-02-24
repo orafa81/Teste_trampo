8a43e3fa57cf7f0159fe57b845843653326ad203
orafa81 <contapedroaraujo@gmail.com>
2025-02-24 01:41:12 -0300

    primeiro commit

--- resources/views/vaga.blade.php
+++ resources/views/vaga.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vaga</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/app.css')
+
 </head>
 
 <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    @vite('resources/css/app.css')
+
 </head>
 
 <body>
